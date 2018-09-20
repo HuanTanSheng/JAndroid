728ec5236dafa992b49fe6de60b598f2c795e0c5
HuanYiWen <35919502@qq.com>
2018-09-20 09:58:59 +0800

    coding

--- jandroid/src/main/java/com/huantansheng/jandroid/constants/JCode.kt
+++ jandroid/src/main/java/com/huantansheng/jandroid/constants/JCode.kt
@@ -5,12 +5,15 @@ package com.huantansheng.jandroid.constants
  * Created by huan on 2017/7/27.
  */
 
-object JCode {
-    //权限请求码
-    const val REQUEST_PERMISSION = 1000
+class JCode {
+    companion object {
+        //权限请求码
+        const val REQUEST_PERMISSION = 1000
 
-    //跳转系统界面
-    const val REQUEST_SETTING_APP_DETAILS = 2001
-    const val REQUEST_SETTING_NET = 2002
+        //跳转系统界面
+        const val REQUEST_SETTING_APP_DETAILS = 2001
+        const val REQUEST_SETTING_NET = 2002
+    }
+
 
 }
--- jandroid/src/main/java/com/huantansheng/jandroid/constants/JKey.kt
+++ jandroid/src/main/java/com/huantansheng/jandroid/constants/JKey.kt
@@ -5,9 +5,12 @@ package com.huantansheng.jandroid.constants
  * Created by huan on 2017/7/27.
  */
 
-object JKey {
+class JKey {
 
-    //是否是首次启动app
-   const val IS_FIRST_LAUNCH_APP = "isFirstLaunchApp"
+    companion object {
+        //是否是首次启动app
+        const val IS_FIRST_LAUNCH_APP = "isFirstLaunchApp"
+    }
+
 
 }
