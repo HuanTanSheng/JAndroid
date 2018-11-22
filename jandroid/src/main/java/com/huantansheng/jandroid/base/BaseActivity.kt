6174ca6a78f6006d84e8b9b8518095a548825a86
HuanYiWen <35919502@qq.com>
2018-11-22 14:58:46 +0800

    baseActivity

--- jandroid/src/main/java/com/huantansheng/jandroid/base/BaseActivity.kt
+++ jandroid/src/main/java/com/huantansheng/jandroid/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.huantansheng.jandroid.base
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
 @SuppressLint("Registered")
@@ -20,6 +21,13 @@ open class BaseActivity : RxAppCompatActivity() {
     override fun onDestroy() {
         BaseApplication.instance.actOut(this)
         super.onDestroy()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
